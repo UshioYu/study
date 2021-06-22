88eb4719b0c168fdc2fb42f649f018507c3a8a0f
yujian <yujian@ysten.com>
2021-06-22 22:49:11 +0800

    修复编译错误

--- src/main/java/com/ushio/wechat/util/Constants.java
+++ src/main/java/com/ushio/wechat/util/Constants.java
@@ -4,7 +4,7 @@ package com.ushio.wechat.util;
  * @author: ushio
  * @description:
  **/
-public class Constant {
+public class Constants {
     //selenium相关
     public static final String BASEURL = "https://work.weixin.qq.com/wework_admin/frame";
 }
